756ac13fa04425571a0f1e2fba1abd52e4a76921
Victor Asido <138793005+victorasido@users.noreply.github.com>
2026-03-30 14:49:50 +0700

    Delete debug.py

--- debug.py
+++
@@ -1,9 +0,0 @@
-from docx import Document
-
-doc = Document("Technical_Spesification_Document_(TSD)_-_MPM_Dom_(CR46881).docx")  # ganti dengan nama file DOCX lo
-for t_idx, table in enumerate(doc.tables):
-    for r_idx, row in enumerate(table.rows):
-        for c_idx, cell in enumerate(row.cells):
-            lines = [p.text.strip() for p in cell.paragraphs if p.text.strip()]
-            if lines:
-                print(f"[T{t_idx}R{r_idx}C{c_idx}] {repr(' '.join(lines))}")
